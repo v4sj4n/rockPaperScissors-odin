d97713c6653aabd94ec1969a40c75ff38f547821
Vasjan <vasjancupri04@gmail.com>
2022-11-19 13:47:36 +0100

    Done half the part

--- script.js
+++ script.js
@@ -11,19 +11,11 @@ let rounds = document.querySelectorAll(".rounds").forEach((item) => {
     let h3InfoRounds = document.createElement("h3");
     h3InfoRounds.textContent = `You chose best of ${clickedRound}`;
     h3InfoRounds.style.color = "#34cb98";
+    h3InfoRounds.setAttribute('rounds',clickedRound)
     header.appendChild(h3InfoRounds);
 
     let roundsDiv = document.getElementById("round-chooser");
     roundsDiv.style.display = "none";
     return clickedRound;
-  });
-});
-
-let attack = document.querySelectorAll(".rps").forEach((item) => {
-  item.addEventListener("click", () => {
-    let scores = document.querySelectorAll(".score");
-    scores.forEach((score) => {
-      score.style.display = "block";
-    });
   });
 });
